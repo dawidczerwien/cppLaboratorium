efed182b001b7e1ee6c239232217547431b65bb5
Dawid Czerwien <dawid.czerwien@smcebi.edu.pl>
2021-03-24 10:54:36 +0100

    zadanie 3.1

---
+++ kccpZadania/ZadAdresowaniePamieci.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+        int a;
+        int b;
+        int c;
+
+	//adresowanie pamieci
+        unsigned long p = (unsigned long) &a;
+        cout <<"Adres zmiennej A: "<< &a << "\t dec: " << p  << endl;
+
+	p = (unsigned long) &b;
+	cout <<"Adres zmiennej B: "<< &b << "\t dec: " << p  << endl;
+
+	p = (unsigned long) &c;
+        cout <<"Adres zmiennej C: "<< &c << "\t dec: " << p  << endl;
+}
