a8fbe78ede051addf3fd1683fbc7e0ca0eafc87e
Dawid Czerwien <dawid.czerwien@smcebi.edu.pl>
2021-03-30 23:20:22 +0200

    zadanie 3.3

--- kccpZadania/ZadArytmetykaWskaznikow.cc
+++ kccpZadania/ZadArytmetykaWskaznikow.cc
@@ -1,60 +1,96 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int tab[] = {11,22,33,44,55,66,77,88,99}, i = 3, *p, *q;
-	//funkcja 1
-        p = &tab[0];
-        cout << "*p        = " << *p << endl;
+void funkcja1() {
+	int a=11, b=22, c=33, d=44, e=55, f=66, g=77, h=88, *p, *q;
 
-	p++;
-        cout << "p++       = " << *p << endl;
+	p = &a;
+	cout << "Funkcja 1 -------------------------------------" << endl;
+	//post inkrementacja adresu
+        cout << "p++    = " << *p <<", ref: " << p++ << ", val aftef: "<< *p <<  endl;
 
-	++p;
-        cout << "++p       = " << *p << endl;
+        //pre inkrementacja adresu
+        cout << "++p    = " << *p <<", ref: " << ++p << ", val aftef: "<< *p << endl;
 
-	++*p;
-        cout << "++*p      = " << *p << endl;
+        //pre inkrementacja wartości w tablicy pod wskaźnikiem
+        cout << "++*p   = " << ++*p << ", ref: " << p << ", val aftef: "<< *p << endl;
 
-	++(*p);
-        cout << "++(*p)    = " << *p << endl;
+        //to samo co wyżej
+        cout << "++(*p) = " << ++(*p) << ", ref: " << p << ", val after: " << *p << endl;
 
-	++*(p);
-        cout << "++*(p)    = " << *p << endl;
+        //to samo co wyżej
+        cout << "++*(p) = " << ++*(p)  << ", ref: " << p << ", val after: " << *p << endl;
 
-        *p++;
-        cout << "*p++      = " << *p << endl;
+        //post inkrementacja adresu
+        cout << "*p++   = " << *p++ << ", ref: " << p << ", val after: " << *p << endl;
 
-	(*p)++;
-	cout << "*(p)++    = " << *p << endl;
+	//post inkrementacja wartości w tablicy pod wskaźnikiem
+	cout << "(*p)++ = " << (*p)++ << ", ref: " << p << ", val after: " << *p << endl;
 
-	*(p)++;
-        cout << "*(p)++    = " << *p << endl;
+	//pre inkrementacja wartości w tablicy pod wskaźnikiem
+	cout << "++*(p) = " << ++*(p) << ", ref: " << p << ", val after: " << *p << endl;
 
-	*++p;
-        cout << "*++p      = " << *p << endl;
+        //pre inkrementacja adresu
+	cout << "*++p   = " << *++p << ", ref: " << p << ", val after: " << *p << endl;
 
-	*(++p);
-        cout << "*(++p)    = " << *p << endl;
+	//pre inkrementacja adresu
+	cout << "*(++p) = " << *(++p) << ", ref: " << p << ", val after: " << *p << endl;
+}
+void funkcja2() {
+int tab[] = {11,22,33,44,55,66,77,88}, i = 3, *p, *q;
 
-	//funkcja 2
+	p = &tab[0];
+	cout << "Funkcja 2 -------------------------------------" << endl;
+	//wskazuje na pierwszy element w tablicy
+	cout << "*p     = " << *p << ", ref: " << p << endl;
+
+	//post inkrementacja adresu
+	cout << "p++    = " << *p <<", ref: " << p++ << ", val aftef: "<< *p <<  endl;
+
+	//pre inkrementacja adresu
+	cout << "++p    = " << *p <<", ref: " << ++p << ", val aftef: "<< *p << endl;
+
+	//pre inkrementacja wartości w tablicy pod wskaźnikiem
+	cout << "++*p   = " << ++*p << ", ref: " << p << ", val aftef: "<< *p << endl;
+
+	//to samo co wyżej
+	cout << "++(*p) = " << ++(*p) << ", ref: " << p << ", val after: " << *p << endl;
+
+	//to samo co wyżej
+	cout << "++*(p) = " << ++*(p)  << ", ref: " << p << ", val after: " << *p << endl;
+
+	//post inkrementacja adresu
+	cout << "*p++   = " << *p++ << ", ref: " << p << ", val after: " << *p << endl;
+
+	//post inkrementacja wartości w tablicy pod wskaźnikiem
+	cout << "(*p)++ = " << (*p)++ << ", ref: " << p << ", val after: " << *p << endl;
+
+	//pre inkrementacja wartości w tablicy pod wskaźnikiem
+	cout << "++*(p) = " << ++*(p) << ", ref: " << p << ", val after: " << *p << endl;
+
+	//pre inkrementacja adresu
+	cout << "*++p   = " << *++p << ", ref: " << p << ", val after: " << *p << endl;
+
+	//pre inkrementacja adresu
+	cout << "*(++p) = " << *(++p) << ", ref: " << p << ", val after: " << *p << endl;
+}
+
+void funkcja3() {
 	int a, b, c;
 	float x;
 	int *wsk_int;
+	cout << "Funkcja 3 -------------------------------------" << endl;
 	a = b = c = 0;
-
 	cout <<"1. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
-	b = 10;
+        b = 10;
 	cout <<"2. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
-
 	wsk_int = &b;
 	*(++wsk_int) = 20;
 	cout <<"3. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
-
 	wsk_int = &a;
 	*(wsk_int+1) = 30;
-	cout <<"4. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
+        cout <<"4. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
         *(&a) = 40;
         cout <<"5. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
@@ -63,14 +99,19 @@ int main() {
         cout <<"6. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
         *( (int*)&x - 1) = 60;
-        cout <<"7. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
-
+	cout <<"7. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
         *(int*)( &x - 2) = 70;
         cout <<"8. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
-
-        *((int*)(wsk_int)) = 80;
+	*((int*)(wsk_int)) = 80;
         cout <<"9. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
-
         *(int*) (wsk_int + 1) = 90;
         cout <<"10. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
+}
+int main() {
+	funkcja1();
+
+	funkcja2();
+
+	funkcja3();
+
 }
