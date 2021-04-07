eaf2c655d1902b3142d0b3b6718d93a972536408
Dawid Czerwien <dawid.czerwien@smcebi.edu.pl>
2021-04-07 10:34:21 +0200

    zadanie 4.3

--- kccpZadania/ZadParzysta.cc
+++ kccpZadania/ZadParzysta.cc
@@ -4,8 +4,10 @@ using namespace std;
 void funkcjaA(int x) {
 	// 1 & liczba parzysta zwraca 0
 	// 1 & liczba nie parzysta zwraca 1
-	// 1 & 11 zwraca 1
-	// 1 & 10 zwraca 0
+	// Przykład dla liczby 7
+	// 1 & 111 zwraca 1
+	// Przykład dla liczbt 8
+	// 1 & 1000 zwraca 0
         if(x & 1) cout <<"Liczba nieparzysta."<< endl;
         else cout <<"Liczba parzysta."<< endl;
 }
@@ -13,6 +15,10 @@ void funkcjaA(int x) {
 void funkcjaB(int x) {
 	if(x % 2 == 1) cout <<"Liczba nieparzysta."<< endl;
 	else cout <<"Liczba parzysta."<< endl;
+}
+
+void funkcjaC(int x) {
+        cout << ((x % 2 == 1) ? "Liczba nieparzysta." : "Liczba parzysta.")<< endl;
 }
 
 int main()
@@ -25,5 +31,8 @@ int main()
 
 	cout << "Wynik funkcji B: " << endl;
         funkcjaB(x);
+
+	cout << "Wynik funkcji C: " << endl;
+        funkcjaC(x);
 	return 0;
 }
