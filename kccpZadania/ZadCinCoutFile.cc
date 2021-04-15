2f6494fae7f79812e3dd64a552f18ab1c2ee7a14
Dawid Czerwien <dawid.czerwien@smcebi.edu.pl>
2021-04-15 19:09:50 +0200

    zadanie 4.1

---
+++ kccpZadania/ZadCinCoutFile.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+int main() {
+	cout << "cout output"<< endl;
+	// ./a.out 2>error.txt - zapisanie błedu do pliku
+	cerr << "Strumień błędów"<< endl;
+	// zapis do pliku
+	ofstream zapisz("tekst.txt");
+	zapisz << "Tekst w pliku" << endl;
+	zapisz.close();
+	// odczyt z pliku
+	ifstream czytaj("tekst.txt");
+	string a, b, c;
+	czytaj>>a>>b>>c;
+	cout << a << " " << b << " " << c << endl;
+	czytaj.close();
+	return 0;
+}
